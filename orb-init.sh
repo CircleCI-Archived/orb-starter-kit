60739e5335496961d2007ced8be1602b31456b08
Kyle Tryon <33272306+KyleTryon@users.noreply.github.com>
2019-07-17 20:39:41 -0400

    pushing changes for day

--- orb-init.sh
+++ orb-init.sh
@@ -8,13 +8,12 @@ echo "Installing CircleCI CLI"
 curl -fLSs https://circle.ci/cli | sudo bash && circleci setup
 sleep 1
 echo
-CCI_TOKEN=$(grep -Po "(?<=token: ).*" $HOME/.circleci/cli.yml)
+CCI_TOKEN=$(grep -Po "(?<=token: ).*" "$HOME"/.circleci/cli.yml)
 echo -e "\e[1mBegin Orb Creation\e[0m"
 echo
 sleep 1
 echo -e "\e[1mEnter your GitHub Organization name\e[0m"
-read -p "Enter GitHub Orginzation name: " -r CCI_ORGINIZATION
-echo
+read -p "Enter GitHub Orginzation name: " -r CCI_ORGANIZATION
 _checkRepoName() {
     echo
     echo -e "\e[1mThe repoistory name is currently set to \e[36m$(basename "$PWD")\e[39m is this correct?\e[0m"
@@ -31,24 +30,26 @@ _checkRepoName() {
     fi
 }
 _checkRepoName
-sleep 1
-exec < /dev/tty
 echo
-_setNamespace() {
-    exec < /dev/tty
-    echo
-    echo -e "\e[1mSelect your Orb namespace. Each organization/user may claim one unique namespace.\e[0m"
-    read -p "Enter Namespace: " -r CCI_NAMESPACE
-    echo
-    echo -e "You have selected \e[1m${CCI_NAMESPACE}\e[0m"
-    read -p "Are you sure? " -n 1 -r
-    echo
-    if [[ ! $REPLY =~ ^[Yy]$ ]]
-    then
-        _setNamespace
-    fi
-}
-_setNamespace
+echo "Initializing git"
+git init
+git remote add origin git@github.com:"$CCI_ORGANIZATION"/"$CCI_REPO".git
+git add .
+git commit -m "initial commit"
+git push -u origin master
+sleep 1
+echo "Create a GitHub Personal access token"
+echo "The access token will be used to add a public key to your account automatically"
+echo
+sleep 1
+echo "Create your peronal access token here: https://github.com/settings/tokens"
+sleep 3
+read -p "Enter GitHub Personal Access Token: " -r CCI_GH_TOKEN
+echo
+read -r # This read is here to fix a glitch where the next read was not waiting to accept input. This needs to be resolved but this workaround is functional
+echo
+echo -e "\e[1mSelect your Orb namespace. Each organization/user may claim one unique namespace.\e[0m"
+read -p "Enter Namespace: " -r CCI_NAMESPACE
 sleep 1
 circleci namespace create "$CCI_NAMESPACE" github "$CCI_ORGANIZATION"
 sleep 1
@@ -56,7 +57,15 @@ echo
 echo -e "\e[1mSelect your Orb Name. Your Orb will live at ${CCI_NAMESPACE}/{ORB NAME}\e[0m"
 read -p "Enter Orb Name: " -r CCI_ORBNAME
 echo
+sleep 1
 echo "Orb ${CCI_NAMESPACE}/${CCI_ORBNAME} selected"
+echo "Creating Orb"
+sleep 2
+echo
+circleci orb create "${CCI_NAMESPACE}/${CCI_ORBNAME}"
+echo
+sleep 1
+git add 
 echo
 echo "Creating deployment key."
 sleep 2
@@ -64,18 +73,26 @@ echo "The private key will automatically be added to the CircleCI repository for
 ssh-keygen -t rsa -b 4096 -m PEM -N "" -f "${CCI_ORBNAME}-key"
 echo
 echo "${CCI_ORBNAME}-key keypair has been created"
+CCI_FINGERPRINT=$(ssh-keygen -E md5 -lf "${CCI_ORBNAME}-key" | grep -Po "(?<=MD5:).+?(?=\s)")
+echo "Private key fingerprint: $CCI_FINGERPRINT"
 echo
 sleep 1
 echo "Adding project to CircleCI"
-sleep 0.5
-echo "This will result in a failing job momentarily while we continue to configure your project."
-echo "Please disreguard these failures."
-echo
 sleep 2
-curl -X POST https://circleci.com/api/v1.1/project/github/${CCI_ORGINIZATION}/${CCI_REPO}/follow?circle-token=${CCI_TOKEN}
+# Follow project on CircleCI
+curl -X POST https://circleci.com/api/v1.1/project/github/"${CCI_ORGANIZATION}"/"${CCI_REPO}"/follow?circle-token="${CCI_TOKEN}"
 echo "Project added to CircleCI"
 echo
 sleep 1
-echo "Adding deployment key to CircleCI"
-CCI_FINGERPRINT=$(curl -X POST --header "Content-Type: application/json" -d '{"hostname":"github.com","private_key":"$(cat ${CCI_ORBNAME}-key"}' "https://circleci.com/api/v1.1/project/github/${CCI_ORGANIZATION}/${CCI_REPO}/ssh-key?circle-token=${CCI_TOKEN}" | jq '.fingerprint')
-echo -e "Deployment key added. Fingerprint: \e[31m$CCI_FINGERPRINT\e[39m"
+echo "Adding private key to CircleCI"
+CCI_ORB_KEY=$(cat "${CCI_ORBNAME}-key")
+curl -X POST --header "Content-Type: application/json" -d '{"hostname":"github.com","private_key":"'"$(cat "$CCI_ORBNAME-key")"'"}' "https://circleci.com/api/v1.1/project/github/"${CCI_ORGANIZATION}"/"${CCI_REPO}"/ssh-key?circle-token="${CCI_TOKEN}""
+echo "Adding public key to GitHub"
+curl --silent -u "$CCI_ORGANIZATION:$CCI_GH_TOKEN" https://api.github.com/user -X POST --header "Content-Type: application/json" -d '{"title":"orb-deploy","key":"'"$(cat "$CCI_ORBNAME-key.pub")"'","read_only":false}' "https://api.github.com/repos/"${CCI_ORGANIZATION}"/"${CCI_REPO}"/keys"
+echo  "Keys added"
+echo
+echo "Modifying config template"
+_editConfig() {
+    
+}
+

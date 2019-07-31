b288ed7c889e78c874f81fd51db743fe923de0b2
Kyle Tryon <Kyle.Tryon@outlook.com>
2019-07-31 16:56:21 -0400

    lint

--- _init/_setCreateRepo.sh
+++ _init/_setCreateRepo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 _setCreateRepo() {
     GIT_REPO_CREATE_RES=$(curl -u "$CCI_ORGANIZATION":"$CCI_GH_TOKEN" --silent --write-out "HTTPSTATUS:%{http_code}" https://api.github.com/user/repos -X POST --header "Content-Type: application/json" -d '{"name":"'"$CCI_REPO"'","read_only":false}')
-    GIT_RES_BODY=$(echo $GIT_REPO_CREATE_RES | sed -e 's/HTTPSTATUS\:.*//g')
-    GIT_RES_STATUS=$(echo $GIT_REPO_CREATE_RES | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+    GIT_RES_BODY=$(echo "$GIT_REPO_CREATE_RES" | sed -e 's/HTTPSTATUS\:.*//g')
+    GIT_RES_STATUS=$(echo "$GIT_REPO_CREATE_RES" | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
     if [ "$GIT_RES_STATUS" == "201" ]
     then
         echo

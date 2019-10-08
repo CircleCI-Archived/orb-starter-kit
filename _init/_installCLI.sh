94138acd0f13003c403a8589743886f5fedaf3ea
sashman <sashman90@gmail.com>
2019-10-08 11:21:57 +0100

    Checking for existing circleci execuatble and using the output of diagnostic command #16

--- _init/_installCLI.sh
+++ _init/_installCLI.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 _installCLI() {
-    curl -fLSs https://circle.ci/cli | sudo bash && circleci setup
-    sleep 1
-    echo
-    echo "setting token internally"
-    CCI_TOKEN=$(awk '/token:/ {print $2}' "$HOME/.circleci/cli.yml")
+    if [ -x "$(command -v circleci)" ]; then
+        echo "circleci cli already installed"
+        circleci setup
+        echo "setting token internally"
+        CLI_FILE_PATH=$(circleci diagnostic | grep "Config found:" | awk '{print $3}')        
+    else
+        echo "circleci cli not installed"
+        curl -fLSs https://circle.ci/cli | sudo bash && circleci setup
+        sleep 1
+        echo
+        echo "setting token internally"
+        CLI_FILE_PATH="$HOME/.circleci/cli.yml"
+    fi
+
+    echo $CLI_FILE_PATH
+    CCI_TOKEN=$(awk '/token:/ {print $2}' "$CLI_FILE_PATH")
 }
+
+_installCLI
